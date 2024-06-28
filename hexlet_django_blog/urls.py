9817b2e276cf527caf1891615ce5cee15feac75a
Yulia <jilly-2005@yandex.ru>
2024-06-28 13:47:54 +0300

    added index.html, about.html

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import views
 
 urlpatterns = [
+    path('', views.index),
+    path('about/', views.about),
     path('admin/', admin.site.urls),
 ]
