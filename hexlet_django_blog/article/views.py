ab2350bdc0f26caafc56fa5a6d937170e4c80afc
Yulia <jilly-2005@yandex.ru>
2024-07-02 19:00:17 +0300

    admin edited

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -11,11 +11,3 @@ class IndexView(View):
 def index(request, tags, article_id):
     context = {"tags": tags, "article_id": article_id}
     return render(request, 'article.html', context)
-
-
-"""
-Сделайте так, чтобы hexlet_django_blog.article.views.index 
-принимала строковый параметр "tags" и целочисленный параметр 
-"article_id" из пути /articles/tags/article_id и выводила текст 
-в виде Статья номер 42. Тег python
-"""
